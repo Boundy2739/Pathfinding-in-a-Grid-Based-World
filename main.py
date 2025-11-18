ef2f0a23fd8d23934b58694d9ebc64089236d010
Boundy2739 <boundy42@gmail.com>
2025-11-18 16:00:47 +0000

    Simple maze with a start and an exit

--- main.py
+++ main.py
@@ -1,59 +1,77 @@
 #the code currently changes all the 0 in the rows into a 1 until it reaches a 5 which is the finish line for the time
 import random
+import time
+import os
 
-def create_grid(start,finish,wall):
+def create_grid(entrance,finish,wall):
     grid = [
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
     [0,2,2,2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2,2],
     [0,0,0,0,0,0,0,0,2,2,2,2,2,2,0,2,2,2,2,2],
-    [2,2,2,2,2,2,2,2,2,0,0,0,0,2,0,2,2,2,2,2],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0],
-    #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2],
+    [2,0,0,0,0,0,0,0,0,0,2,2,2,2,0,2,2,2,2,2],
+    [2,0,2,2,2,2,2,2,2,0,2,2,2,2,0,0,0,0,0,2],
+    [2,0,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,0,0,2],
+    [2,0,0,0,0,0,2,0,2,0,0,0,0,0,0,0,0,0,0,2],
+    [2,2,2,2,2,0,2,0,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [2,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0],
+    [2,2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2,0,0],
+    [2,0,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,2,0,0],
+    [2,2,2,2,0,2,2,2,2,2,2,2,2,2,0,0,0,2,0,0],
+    [2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
     ]
     
     #temporary starting line
-    grid[start[0]][start[1]] = 1
+    grid[entrance[0]][entrance[1]] = 1
     #temporary finish
     grid[finish[0]][finish[1]] = 5
     #grid = create_wall(grid)
     for rows in grid:
-        print("".join([' ' if cell == 0 else  '█' if cell == 2 else '?' for cell in rows]))
+        print("".join([' ' if cell == 0 else  '█' if cell == 2 else "\033[43m \033[0m" for cell in rows]))
         
         
     return grid
-def move(start,current_grid,rows,columns,finish):
-    while start[1] < columns - 1:
-        if current_grid[start[0]][start[1] + 1] != 2: 
-            start[1] = start[1] + 1
-            current_grid = update_grid(current_grid,start)
-        elif start [0] != 2 and start [0] != 1:
-            start [0] += 1
-            current_grid = update_grid(current_grid,start)
-            
-        elif start[0] - 1 != 2 and start [0] == 1:
-            start [0] -= 1
-            current_grid = update_grid(current_grid,start)
-
-        if start [1] == columns - 1:
-            start [1] = 0
-            if start[0] < rows:
-                start [0] += 1
-            current_grid = update_grid(current_grid,start)
+def move(entrance,current_grid,rows,columns,finish):
+    while entrance[1] < columns - 1:
+        #moves left if there is space
+        while current_grid[entrance[0]][entrance[1] + 1] != 2 and current_grid[entrance[0]][entrance[1] + 1] != 1: 
+            entrance[1] = entrance[1] + 1
+            current_grid = update_grid(current_grid,entrance)
+        #moves down if there is space
+        while current_grid [entrance[0] + 1][entrance[1]] != 2 and current_grid [entrance[0] + 1][entrance[1]] != 1:
+            entrance[0] = entrance[0] + 1
+            current_grid = update_grid(current_grid,entrance)
+        #moves up if there is space
+        while current_grid [entrance[0] - 1][entrance[1]] != 2 and current_grid [entrance[0] - 1][entrance[1]] != 1:
+            entrance[0] = entrance[0] - 1
+            current_grid = update_grid(current_grid,entrance)
+        #moves right if there is space
+        while current_grid[entrance[0]][entrance[1] - 1] != 2 and current_grid[entrance[0]][entrance[1] - 1] != 1: 
+            entrance[1] = entrance[1] - 1
+            current_grid = update_grid(current_grid,entrance)
+        
+        
+        if entrance [1] == columns - 1:
+            entrance [1] = 0
+            if entrance[0] < rows:
+                entrance [0] += 1
+            current_grid = update_grid(current_grid,entrance)
         if  current_grid [finish[0]][finish[1]] == 1:
             print("Over")
             return
             
-    print (start[1])
+    
     
 
-def update_grid(grid,start):
-    grid[start[0]][start[1]] = 1
+def update_grid(grid,entrance):
+    time.sleep(0.1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    grid[entrance[0]][entrance[1]] = 1
     for rows in grid:
-        print("".join([' ' if cell == 0 else  '█' if cell == 2 else '?' for cell in rows]))
+        print("".join([' ' if cell == 0 else  '█' if cell == 2 else "\033[41m \033[0m" for cell in rows]))
     print("")
     print("")
     print("")
@@ -90,12 +108,13 @@ def create_wall(grid):
             print("space taken!!")
     return grid
 
-start = [0,0]
-finish = [3,19]
+entrance = [1,0]
+finish = [14,4]
 wall = [0,1]
 rows = 3
 columns = 20
 
-current_grid = create_grid(start,finish,wall)
-new_point = move(start,current_grid,rows,columns,finish)
+current_grid = create_grid(entrance,finish,wall)
+new_point = move(entrance,current_grid,rows,columns,finish)
+
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-
+        elif current_grid [entrance[0] + 1][entrance[1]] != 2 and current_grid [entrance[0] + 1][entrance[1]] != 1:
+            entrance[0] = entrance[0] + 1
+            current_grid = update_grid(current_grid,entrance)
+        elif current_grid [entrance[0] - 1][entrance[1]] != 2 and current_grid [entrance[0] - 1][entrance[1]] != 1:
+            entrance[0] = entrance[0] - 1
+            current_grid = update_grid(current_grid,entrance)
+        elif current_grid[entrance[0]][entrance[1] - 1] != 2
