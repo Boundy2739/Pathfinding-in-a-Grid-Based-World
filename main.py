79d5b3dc03c643200f67ed958dcaf985c29a3d29
Boundy2739 <boundy42@gmail.com>
2025-11-18 19:46:50 +0000

    Testing pick a path algorithm

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import os
 
 def create_grid(entrance,finish,wall):
     grid = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-    [0,2,2,2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2,2],
-    [0,0,0,0,0,0,0,0,2,2,2,2,2,2,0,2,2,2,2,2],
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2],
-    [2,0,0,0,0,0,0,0,0,0,2,2,2,2,0,2,2,2,2,2],
-    [2,0,2,2,2,2,2,2,2,0,2,2,2,2,0,0,0,0,0,2],
-    [2,0,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,0,0,2],
-    [2,0,0,0,0,0,2,0,2,0,0,0,0,0,0,0,0,0,0,2],
-    [2,2,2,2,2,0,2,0,2,2,2,2,2,2,2,2,2,2,2,2],
-    [2,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [2,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0],
-    [2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0],
-    [2,2,2,2,2,2,2,2,2,2,2,2,0,2,2,2,2,2,0,0],
-    [2,0,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,2,0,0],
-    [2,2,2,2,0,2,2,2,2,2,2,2,2,2,0,0,0,2,0,0],
-    [2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0],
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+    [2,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [2,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,0,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,0,2],
+    [2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     ]
     
     #temporary starting line
@@ -30,39 +30,72 @@ def create_grid(entrance,finish,wall):
     grid[finish[0]][finish[1]] = 5
     #grid = create_wall(grid)
     for rows in grid:
-        print("".join([' ' if cell == 0 else  '█' if cell == 2 else "\033[43m \033[0m" for cell in rows]))
+        print("".join([' ' if cell == 0 else  '█' if cell == 2 else "\033[43m \033[0m"  for cell in rows]))
         
         
     return grid
 def move(entrance,current_grid,rows,columns,finish):
     while entrance[1] < columns - 1:
-        #moves left if there is space
-        while current_grid[entrance[0]][entrance[1] + 1] != 2 and current_grid[entrance[0]][entrance[1] + 1] != 1: 
-            entrance[1] = entrance[1] + 1
-            current_grid = update_grid(current_grid,entrance)
-        #moves down if there is space
-        while current_grid [entrance[0] + 1][entrance[1]] != 2 and current_grid [entrance[0] + 1][entrance[1]] != 1:
-            entrance[0] = entrance[0] + 1
-            current_grid = update_grid(current_grid,entrance)
-        #moves up if there is space
-        while current_grid [entrance[0] - 1][entrance[1]] != 2 and current_grid [entrance[0] - 1][entrance[1]] != 1:
+        #this checks if theres is a free path both up and down picks a random path to follow
+        if current_grid [entrance[0] + 1][entrance[1]] != 2 and current_grid[entrance[0] - 1][entrance[1]] != 2:
+            pick_a_path(current_grid,entrance)
+            if  current_grid [finish[0]][finish[1]] == 1:
+                        print("Exit reached!!!")
+                        return
+        
+        else:
+            
+            #moves left if there is space
+                if current_grid[entrance[0]][entrance[1] + 1] != 2 and current_grid[entrance[0]][entrance[1] + 1] != 1: 
+                    entrance[1] = entrance[1] + 1
+                    current_grid = update_grid(current_grid,entrance)
+                    if current_grid [entrance[0] + 1][entrance[1]] != 2 and current_grid[entrance[0] - 1][entrance[1]] != 2:
+                        pick_a_path(current_grid,entrance)
+                    if  current_grid [finish[0]][finish[1]] == 1:
+                        print("Exit reached!!!")
+                        return
+                #moves down if there is space
+                if current_grid [entrance[0] + 1][entrance[1]] != 2 and current_grid [entrance[0] + 1][entrance[1]] != 1:
+                    entrance[0] = entrance[0] + 1
+                    current_grid = update_grid(current_grid,entrance)
+                    if  current_grid [finish[0]][finish[1]] == 1:
+                        print("Exit reached!!!")
+                        return
+                #moves up if there is space
+                if current_grid [entrance[0] - 1][entrance[1]] != 2 and current_grid [entrance[0] - 1][entrance[1]] != 1:
+                    entrance[0] = entrance[0] - 1
+                    current_grid = update_grid(current_grid,entrance)
+                    if  current_grid [finish[0]][finish[1]] == 1:
+                        print("Exit reached!!!")
+                        return
+                #moves right if there is space
+                if current_grid[entrance[0]][entrance[1] - 1] != 2 and current_grid[entrance[0]][entrance[1] - 1] != 1: 
+                    entrance[1] = entrance[1] - 1
+                    current_grid = update_grid(current_grid,entrance)
+                    if  current_grid [finish[0]][finish[1]] == 1:
+                        print("Exit reached!!!")
+                        return
+                
+                
+                if entrance [1] == columns - 1:
+                    entrance [1] = 0
+                    if entrance[0] < rows:
+                        entrance [0] += 1
+                    current_grid = update_grid(current_grid,entrance)
+                if  current_grid [finish[0]][finish[1]] == 1:
+                    print("Exit reached!!!")
+                    return
+
+def pick_a_path(current_grid,entrance):
+    path = random.choice((1,2))
+    match path:
+        case 1:
+           entrance[0] = entrance[0] + 1
+           current_grid = update_grid(current_grid,entrance)
+        case 2:
             entrance[0] = entrance[0] - 1
             current_grid = update_grid(current_grid,entrance)
-        #moves right if there is space
-        while current_grid[entrance[0]][entrance[1] - 1] != 2 and current_grid[entrance[0]][entrance[1] - 1] != 1: 
-            entrance[1] = entrance[1] - 1
-            current_grid = update_grid(current_grid,entrance)
-        
-        
-        if entrance [1] == columns - 1:
-            entrance [1] = 0
-            if entrance[0] < rows:
-                entrance [0] += 1
-            current_grid = update_grid(current_grid,entrance)
-        if  current_grid [finish[0]][finish[1]] == 1:
-            print("Over")
-            return
-            
+    return            
     
     
 
@@ -71,7 +104,7 @@ def update_grid(grid,entrance):
     os.system('cls' if os.name == 'nt' else 'clear')
     grid[entrance[0]][entrance[1]] = 1
     for rows in grid:
-        print("".join([' ' if cell == 0 else  '█' if cell == 2 else "\033[41m \033[0m" for cell in rows]))
+        print("".join([' ' if cell == 0 else  '█' if cell == 2  else "\033[44m \033[0m" for cell in rows]))
     print("")
     print("")
     print("")
@@ -108,8 +141,8 @@ def create_wall(grid):
             print("space taken!!")
     return grid
 
-entrance = [1,0]
-finish = [14,4]
+entrance = [2,1]
+finish = [5,18]
 wall = [0,1]
 rows = 3
 columns = 20
